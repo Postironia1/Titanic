547db1345be6845ea8522e707212a07cff605f0f
Postironia1 <99862833+Postironia1@users.noreply.github.com>
2024-06-24 16:58:18 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,18 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Titanic',
+    version='0.1.0',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'numpy',
+        'pandas',
+        'scikit-learn',
+        'matplotlib',
+        'seaborn',
+        'torch',
+        'jupyter'
+    ],
+)
 
