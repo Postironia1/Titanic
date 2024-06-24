ec12eccb6331679eb09ced15dd019d1547410575
Postironia1 <99862833+Postironia1@users.noreply.github.com>
2024-06-24 17:06:58 +0300

    Update neural_network.py

--- src/models/neural_network.py
+++ src/models/neural_network.py
@@ -1,1 +1,46 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+
+class NeuralNetwork(nn.Module):
+    def __init__(self, input_size):
+        super(NeuralNetwork, self).__init__()
+        self.fc1 = nn.Linear(input_size, 64)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(64, 32)
+        self.fc3 = nn.Linear(32, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        x = self.relu(x)
+        x = self.fc3(x)
+        x = self.sigmoid(x)
+        return x
+
+def train_model(X_train, y_train, epochs=50, batch_size=32):
+    model = NeuralNetwork(input_size=X_train.shape[1])
+    criterion = nn.BCELoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    
+    for epoch in range(epochs):
+        running_loss = 0.0
+        for i in range(0, len(X_train), batch_size):
+            inputs = torch.tensor(X_train[i:i+batch_size], dtype=torch.float32)
+            labels = torch.tensor(y_train[i:i+batch_size], dtype=torch.float32)
+
+            optimizer.zero_grad()
+
+            outputs = model(inputs)
+            loss = criterion(outputs, labels.view(-1, 1))
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+        print(f'Epoch {epoch+1}/{epochs}, Loss: {running_loss}')
+    
+    return model
 
