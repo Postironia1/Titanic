af171d080113449256ec049735d617e4de615e1b
Postironia1 <99862833+Postironia1@users.noreply.github.com>
2024-06-24 16:50:35 +0300

    Update make_dataset.py

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -23,6 +23,10 @@ def main():
     df_train['Embarked'].fillna(df_train['Embarked'].mode()[0], inplace=True)
     df_train = pd.get_dummies(df_train, columns=['Sex', 'Embarked'], drop_first=True)
     
+    features_list = ['Pclass', 'Sex', 'Age', 'SibSp', 'Parch']
+
+    df_train = df_train[features_list]
+    
     save_data(df_train, processed_train_data_path)
 
     df_test = load_data(raw_data_path)
@@ -31,6 +35,8 @@ def main():
     df_test['Age'].fillna(df_test['Age'].median(), inplace=True)
     df_test['Embarked'].fillna(df_test['Embarked'].mode()[0], inplace=True)
     df_test = pd.get_dummies(df_test, columns=['Sex', 'Embarked'], drop_first=True)
+
+    df_test = df_test[features_list]
     
     save_data(df_test, processed_test_data_path)
 
